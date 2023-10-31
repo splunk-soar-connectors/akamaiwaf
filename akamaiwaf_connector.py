9f4dfda7c038ea658320578873c88054b2991941
achandya <achandya@splunk.com>
2023-10-31 14:44:53 +0530

    pre-commit changes

--- akamaiwaf_connector.py
+++ akamaiwaf_connector.py
@@ -5,9 +5,9 @@
 # Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
 #
 # Phantom App imports
+import ipaddress
 import json
 import sys
-import ipaddress
 
 import phantom.app as phantom
 import requests
